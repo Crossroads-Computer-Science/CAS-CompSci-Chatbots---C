340a8011fc591a39fc925a9c92e8ac4c81b78689
Paul Way <pauleway@gmail.com>
2024-05-01 18:40:32 -0700

    Revert "Add files via upload"
    This reverts commit 9d0932bae7184deee6c8965e8b448ed2b3121416.

--- app.py
+++ app.py
@@ -9,37 +9,95 @@ client = OpenAI()
 
 @app.route('/')
 def index():
+    key = os.environ.get('OPENAI_API_KEY')
+    print(key)
     return render_template('index.html')
 
+@app.route('/batty')
+def batty():
+    return render_template('BatmanBot.html')
 
-@app.route('/progress')
-def progress():
-    return render_template('progress-chat.html')
+# ___________________
+@app.route('/techyT')
+def techyT():
+    return render_template('techyTim.html')
+# ___________________
 
 
-@app.route('/dad')
-def dad_jokes():
-    return render_template('dad-jokes.html')
+@app.route('/aboBot')
+def abobot():
+    key = os.environ.get('OPENAI_API_KEY')
+    print(key)
+    return render_template("aboBot.html")
+
+@app.route('/super')
+def super():
+    return render_template('superman.html')
+# ___________________
+
+@app.route('/cardJS')
+def cardJS():
+    return render_template("cardJS.html")
+@app.route('/harry')
+def harry():
+    return render_template("harry.html")
+@app.route('/comedyJS')
+def comedyJS():
+    return render_template("comedyJS.html")
+@app.route('/triviabot')
+def triviabot():
+    return render_template("triviabot.html")
+@app.route("/GuessWho")
+def guesswho():
+    return render_template("GuessWhoBot.html")
+@app.route("/animate")
+def animate():
+    return render_template("animate.html")
 
 
-@app.route('/koala')
-def card():
-    return render_template('koala.html')
+@app.route("/chatbot")
+def chatbot():
+    return render_template("chatbot.html")
 
+@app.route("/techHelp")
+def techhelp():
+    return render_template("techHelp.html")
+
+@app.route("/language")
+def language():
+    return render_template("language.html")
+
+@app.route("/WiseOldManChat")
+def WiseOldManChat():
+    return render_template("WiseOldManChat.html")
+
+@app.route("/twenty")
+def TwentyQuestions():
+    return render_template("twentyquestions.html")
+
+@app.route("/fun")
+def have_fun():
+    return render_template("fun.html")
+
+@app.route('/song')
+def song():
+    return render_template("SongBot.html")
+
+@app.route('/poland')
+def poland():
+    return render_template("poland.html")
 
 @app.route("/chat")
 def chat_completion():
-    try:
-        prompt = request.args.get('prompt')
-        # parse data
-        messages = json.loads(prompt)
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-        )
-        return str(completion.choices[0].message.content)
-    except Exception as e:
-        return str(e), 500  # Return error message with status code 500
+    prompt = request.args.get('prompt')
+    # parse data
+    messages = json.loads(prompt)
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    return str(completion.choices[0].message.content)
 
 
 if __name__ == '__main__':
